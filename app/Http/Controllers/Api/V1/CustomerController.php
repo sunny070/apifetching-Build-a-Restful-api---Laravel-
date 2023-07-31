b2d7597aa7a0f660c3541c75d7f578e12c10b7a5
Sunny <sunnykumark070@gmail.com>
2023-07-31 16:43:40 +0530

    update

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -18,7 +18,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::all();
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
